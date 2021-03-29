b1e3a1d33e54db2af84f9dd3206fe4d9106a66ff
julietapennini <julietapennini@gmail.com>
2021-03-29 13:36:00 -0300

    CREATE/grid

--- app.js
+++ app.js
@@ -1,125 +1,26 @@
-/*
-===========================================================================
-                            Algoritmo básico
-___________________________________________________________________________
+const grid = document.querySelector('.grid')
+const width = 8
+const squares = []
+const candyColors = [
+    'red',
+    'yellow',
+    'orange',
+    'purple',
+    'green',
+    'blue'
+]
 
-- Crear una grilla con x filas e y columnas con items aleatorios.
-    - Chequear si hay bloques (*).
-    - Si hay bloques, volver a generar grilla.
-
-- Intercambiar 2 posiciones.
-
-- Chequear si hay bloques (*).
-    + Si hay bloques:
-        > buscar bloque horizontal y/o vertical (de 3 o más items repetidos).
-            - Eliminar items dentro del bloque.
-            - Mientras haya items con posiciones vacías por debajo:
-                - Obtener la cantidad de posiciones vacías.
-                - Mover ese item la cantidad de posiciones vacías.
-            - Reyenar las posiciones vacías con items aleatorios.
-
-    + Si NO hay bloques:
-        - Volver a intercambiar posiciones.
-
-===========================================================================
-*/
-
-
-/*      
-Valores de configuración
-___________________________________________________________________________
-*/
-
-// Config
-let duracionAnimacion = 400
-let duracionPartida = 30
-let items = ['🍨','🍫','🍬','🍭','🍩','🍰']
-let tamanioCuadrado = 50
-
-// Juego
-let grilla = []
-let tamanioGrilla = 8
-let segundosRestantes = 0
-let puntos = 0
-let modificadorCombo = 1
-let puedeMover = true
-let timer = null
-let iniciado = false
-
-
-/*      
-Utilidades
-___________________________________________________________________________
-*/
-const $ = (selector) => document.querySelector(selector)
-const $$ = (selector) => document.querySelectorAll(selector)
-
-const segundosAMinutos = (segundos) =>
-    Math.floor(segundos / 60) + ":" + ('0' + Math.floor(segundos % 60)).slice(-2)
-
-
-/*      
-DOM
-___________________________________________________________________________
-*/
-
-const actualizarPuntos = () => {
-    $('#score').innerHTML = score
-  }
-  
-const incrementarCombo = () => {
-    modificadorCombo++
-    $('#combo').innerHTML = modificadorCombo
-}
-  
-const reiniciarCombo = () => {
-    modificadorCombo = 1
-    $('#combo').innerHTML = modificadorCombo
-}
-  
-const actualizarTiempoRestante = () => {
-    const tiempoRestante = $('.time-left')
-    tiempoRestante.innerHTML = segundosAMinutos(segundosRestantes)
-}
-
-/*      
-GRILLA
-___________________________________________________________________________
-*/
-const generarGrilla = () => {
-    grilla = []
-
-    for (let i = 0; i < tamanioGrilla; i++) {
-        grilla [i]= []
-        for (let j = 0; j < tamanioGrilla; j++){
-            grilla[i][j] = obtenerItemRamdon ()
-        }
+// Create Board
+function createBoard() {
+    for (let i = 0; i < width*width; i++) {
+        const square = document.createElement('div')
+        square.setAttribute('draggable', true)
+        square.setAttribute('id', i)
+        let randomColor = Math.floor(Math.random () * candyColors.length)
+        square.style.backgroundColor = candyColors[randomColor]
+        grid.appendChild(square)
+        squares.push(square)
     }
 }
 
-const vaciarGrilla = () => ($('#grid').innerHTML = '')
-
-const redimensionarGrilla = () =>
-  ($('#grid').style.height = `${tamanioGrilla * tamanioCuadrado}px`)
-
-
-const dibujarItems = () => {
-    const $grid = $('grid')
-
-    for (let i = 0; i < grilla.length; i++) {
-        for (let j = 0; j < grilla[i].length; j++){
-            if (grid[i][j]) {
-                const cuadrado = generarCuadrado (j,i)
-
-                $grid.appendChild(cuadrado)
-            }
-        }
-    }
-}
-
-const dibujarGrilla = () => {
-    actualizarTamanioCuadrado ()
-    vaciarGrilla ()
-    dibujarItems ()
-    twemoji.parse(document.body)
-}
+createBoard()
